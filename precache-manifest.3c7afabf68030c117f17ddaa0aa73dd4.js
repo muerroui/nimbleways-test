cc4e330c3f979c081e73345fcf6758bb07ac9e3c
muerroui <mustapha.errouissi@capgemini.com>
2019-12-09 10:18:37 +0100

    Updates

---
+++ precache-manifest.3c7afabf68030c117f17ddaa0aa73dd4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63a25ea8568d2c00477be0e22fc583bf",
+    "url": "/nimbleways-test/index.html"
+  },
+  {
+    "revision": "43b2b6f81131edae02c7",
+    "url": "/nimbleways-test/static/css/2.7ec00178.chunk.css"
+  },
+  {
+    "revision": "26075c20d5336cb5774c",
+    "url": "/nimbleways-test/static/css/main.56f28b58.chunk.css"
+  },
+  {
+    "revision": "43b2b6f81131edae02c7",
+    "url": "/nimbleways-test/static/js/2.3c8e191c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/nimbleways-test/static/js/2.3c8e191c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "26075c20d5336cb5774c",
+    "url": "/nimbleways-test/static/js/main.bf7f276e.chunk.js"
+  },
+  {
+    "revision": "206b523f846bce6a8d19",
+    "url": "/nimbleways-test/static/js/runtime-main.a17ef227.js"
+  }
+]);
